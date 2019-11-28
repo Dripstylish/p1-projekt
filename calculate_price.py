d0d4282e8af2c9b4b3130a680ad12dfab9b1e205
Anne C. Haugaard <hva.ska.jeg.hedde@gmail.com>
2019-11-28 11:33:10 +0100

    Fundet hældning og testet

--- calculate_price.py
+++ calculate_price.py
@@ -1,16 +1,7 @@
-import csv
-import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
-from collections import Counter
-
-import math_methods as mm
 
 csv_file_path = "home/HOME.csv"
 
-def import_csv_file():
-    csv_file = pd.read_csv('home/HOME.csv', delimiter=';')
+def import_csv_file(csv):
+    csv_file = pd.read_csv(csv, delimiter=';')
     return csv_file
-
-
-dataset = import_csv_file()
--- math_methods.py
+++ math_methods.py
@@ -1,2 +1,40 @@
-def hældningen():
+"""
+Input: To lister
+
+hældning(x, y)
+
+hældningen(liste1, liste2):
+
+  liste_sq = []
+  for loop liste1:
+    liste_sq.append(liste1[x]^2)
+
+  return (len(liste1) * sum(liste1 * liste2)) - (sum(liste1) * sum(liste2)) / ((len(liste1) * sum(liste_sq)) - (liste1)^2)
+
+Output: Hældningen (et tal)
+"""
+
+def haeldning(dataframe1, dataframe2):
+    """
+    :param dataframe1: variabel x
+    :param dataframe2: variabel y
+    :return: hældningen
+    """
+    power = dataframe1.pow(2)
+    mul = dataframe1.mul(dataframe2)
+    n = len(dataframe1)
+
+    return int((n * mul.sum() - dataframe1.sum() * dataframe2.sum()) / (n * power.sum() - (dataframe1.sum()**2)))
+
+"""
+Input: Usorteret liste af hældninger
+
+aritmetisk_middelværdi(hældningsliste):
+
+  return sum(hældningsliste))/(len(hældningsliste)
+
+Output: Middelværdi (et tal)
+"""
+
+def aritmetisk_middelvaerdi(list1):
     return
